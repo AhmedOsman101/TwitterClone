2f9ccaf74fec67deacb4d0d18f4731788f080809
AhmedOsman101 <trollingosman101@outlook.com>
2024-04-23 10:01:19 +0200

    update 3 files and create 1 file

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,17 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
+Route::get(
+    'register',
+    function () {
+        return Inertia::render('Register');
+    }
+)->name('register');
+
 Route::post(
     'register',
     [UserController::class, 'register']
-)->name('register');
+);
 
 Route::get(
     'login',
