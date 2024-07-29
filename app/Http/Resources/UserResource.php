3d97832a9521a3604c8712f69c1de8545a086a87
AhmadOthman <trollingosman101@outlook.com>
2024-07-30 02:00:11 +0300

    - Remove unused Carbon import from AuthUserResource - Delete ProfileUserResource and merge its functionality into UserResource - Update UserResource to handle user data and notifications - Clean up UserResource to exclude old notification processing

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,6 +6,20 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property int $id
+ * @property string $full_name
+ * @property string $username
+ * @property string $email
+ * @property string $bio
+ * @property string $cover_photo
+ * @property string $profile_picture
+ * @property int $followers_count
+ * @property int $following_count
+ * @property string $created_at
+ * @property \Illuminate\Database\Eloquent\Collection $followers
+ * @property \Illuminate\Database\Eloquent\Collection $following
+ */
 class UserResource extends JsonResource {
   /**
    * Transform the resource into an array.
@@ -13,20 +27,6 @@ class UserResource extends JsonResource {
    * @return array<string, mixed>
    */
   public function toArray(Request $request): array {
-    $allNotifications = NotificationResource::collection($this->notifications)->resolve();
-    $unreadNotifications = NotificationResource::collection($this->unreadNotifications)->resolve();
-
-    // Extract the IDs from the notifications
-    $allNotificationIds = array_column($allNotifications, 'id');
-    $unreadNotificationIds = array_column($unreadNotifications, 'id');
-
-    // Find the IDs that are in $allNotifications but not in $unreadNotifications
-    $readNotificationIds = array_diff($allNotificationIds, $unreadNotificationIds);
-
-    // Filter the $allNotifications array to get only the read notifications
-    $readNotifications = array_filter($allNotifications, function ($notification) use ($readNotificationIds) {
-      return in_array($notification['id'], $readNotificationIds, true);
-    });
     return [
       "id"              => $this->id,
       "full_name"       => $this->full_name,
@@ -35,11 +35,10 @@ class UserResource extends JsonResource {
       "bio"             => $this->bio,
       "cover_photo"     => $this->cover_photo,
       "profile_picture" => $this->profile_picture,
-      "notifications"   => [
-        "all"    => $allNotifications,
-        "read"   => $readNotifications,
-        "unread" => $unreadNotifications,
-      ],
+      // "followers"       => ShortUserResource::collection($this->followers->pluck('follower'))->resolve(),
+      "followers_count" => $this->followers_count,
+      // "following"       => ShortUserResource::collection($this->following->pluck('followed'))->resolve(),
+      "following_count" => $this->following_count,
       "created_at"      => (new Carbon($this->created_at))->isoFormat('MMMM Y'),
     ];
   }
