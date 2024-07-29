fb26e7b45bbbae79cd4afa70f1e0d10231efa211
AhmadOthman <trollingosman101@outlook.com>
2024-07-30 01:57:50 +0300

    - Add index method to NotificationController for retrieving notifications - Implement JSON response with all, unread, and read notifications - Extract and filter read notifications from all notifications - Refactor update and destroy methods to use show method for fetching notifications - Rename `getNotification` to `show` for clarity

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,22 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\NotificationResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller {
+
+  public function index(Request $request): JsonResponse {
+
+    $user = $request->user();
+
+    $allNotifications = NotificationResource::collection($user->notifications)->resolve();
+    $unreadNotifications = NotificationResource::collection($user->unreadNotifications)->resolve();
+
+    // Extract the IDs from the notifications
+    $allNotificationIds = array_column($allNotifications, 'id');
+    $unreadNotificationIds = array_column($unreadNotifications, 'id');
+
+    // Find the IDs that are in $allNotifications but not in $unreadNotifications
+    $readNotificationIds = array_diff($allNotificationIds, $unreadNotificationIds);
+
+    // Filter the $allNotifications array to get only the read notifications
+    $readNotifications = array_filter($allNotifications, function ($notification) use ($readNotificationIds) {
+      return in_array($notification['id'], $readNotificationIds, true);
+    });
+
+    return response()->json([
+      'all' => $allNotifications,
+      'unread' => $unreadNotifications,
+      'read' => $readNotifications,
+    ]);
+  }
+
   public function update(Request $request): void {
-    $notification = $this->getNotification($request);
+    $notification = $this->show($request);
 
     $notification->markAsRead();
   }
 
   public function destroy(Request $request): void {
-    $notification = $this->getNotification($request);
+    $notification = $this->show($request);
 
     $notification->delete();
   }
 
-  private function getNotification(Request $request) {
+  private function show(Request $request) {
     $notifications = $request->user()->notifications;
 
     $notificationIndex = array_search($request->id, array_column($notifications->toArray(), 'id'), true);
