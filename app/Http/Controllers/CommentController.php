78b859aa550d99b0eafd1eb63aeb0210b8ffa812
AhmadOthman <trollingosman101@outlook.com>
2024-07-30 01:31:04 +0300

    Refactor CommentController for consistency and clarity
    - Removed extra spaces in method definitions.
    - Aligned chained method calls for better readability.
    - Updated 'store' method to use Auth::id() for user_id and directly use request properties.
    - Minor reformatting for better code style adherence.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,25 +17,25 @@ class CommentController extends Controller {
   /**
    * Display a listing of the resource.
    */
-  public function index (): string {
+  public function index(): string {
     $temp = Comment::where('tweet_id', request('tweet_id'))
-                   ->with('user')
-                   ->withCount('likes')
-                   ->orderBy('id', 'desc') // Sort chronologically in descending order
-                   ->get();
+      ->with('user')
+      ->withCount('likes')
+      ->orderBy('id', 'desc') // Sort chronologically in descending order
+      ->get();
 
     if ($temp !== null) {
       $likedCommentsIds = Like::select(['id', 'comment_id'])
-                              ->where('user_id', request('user_id'))
-                              ->whereNotNull('comment_id')
-                              ->get()
-                              ->toArray();
+        ->where('user_id', request('user_id'))
+        ->whereNotNull('comment_id')
+        ->get()
+        ->toArray();
 
       $this->setHaystack($likedCommentsIds);
 
       foreach ($temp as &$comment) {
         $comment->liked = $this->isLiked(
-          needle    : $comment->id,
+          needle: $comment->id,
           column_key: "comment_id"
         );
       }
@@ -44,27 +44,27 @@ class CommentController extends Controller {
     return CommentResource::collection($temp)->toJson();
   }
 
-  public function getProfileReplies (int $user_id): array {
+  public function getProfileReplies(int $user_id): array {
 
     $temp = Comment::where('user_id', $user_id)
-                   ->with('user')
-                   ->withCount('likes')
-                   ->orderBy('id', 'desc') // Sort chronologically in descending order
-                   ->get();
+      ->with('user')
+      ->withCount('likes')
+      ->orderBy('id', 'desc') // Sort chronologically in descending order
+      ->get();
 
 
     if ($temp !== null) {
       $likedCommentsIds = Like::select(['id', 'comment_id'])
-                              ->where('user_id', $user_id)
-                              ->whereNotNull('comment_id')
-                              ->get()
-                              ->toArray();
+        ->where('user_id', $user_id)
+        ->whereNotNull('comment_id')
+        ->get()
+        ->toArray();
 
       $this->setHaystack($likedCommentsIds);
 
       foreach ($temp as &$comment) {
         $comment->liked = $this->isLiked(
-          needle    : $comment->id,
+          needle: $comment->id,
           column_key: "comment_id"
         );
       }
@@ -76,39 +76,44 @@ class CommentController extends Controller {
   /**
    * Store a newly created resource in storage.
    */
-  public function store (Request $request): void {
+  public function store(Request $request): void {
     $request->validate(['body' => 'required|max:255|string']);
 
     // user_id, tweet_id, body
-    Comment::create($request->only(['body', 'user_id', 'tweet_id']));
+    Comment::create([
+      "user_id" => Auth::id(),
+      "tweet_id" => $request->tweet_id,
+      "body" => $request->body
+    ]);
 
-    $targetUser = User::find($request->only('target_id'));
 
-    $targetUser->notify(new CommentNotification($request->only('tweet_id')));
+    $targetUser = User::find($request->target_id);
+
+    $targetUser->notify(new CommentNotification($request->tweet_id));
   }
 
   /**
    * Display the specified resource.
    */
-  public function show (Request $request, int $id): string {
+  public function show(Request $request, int $id): string {
 
     $comment = Comment::where('id', $id)
-                      ->with('user')
-                      ->withCount('likes')
-                      ->get();
+      ->with('user')
+      ->withCount('likes')
+      ->get();
 
     if ($comment->isNotEmpty()) {
       $likedCommentsIds = Like::select(['id', 'comment_id'])
-                              ->where('user_id', $request->user_id ?? Auth::user()->id)
-                              ->whereNotNull('comment_id')
-                              ->get()
-                              ->toArray();
+        ->where('user_id', $request->user_id ?? Auth::user()->id)
+        ->whereNotNull('comment_id')
+        ->get()
+        ->toArray();
 
       $this->setHaystack($likedCommentsIds);
 
       foreach ($comment as &$item) {
         $item->liked = $this->isLiked(
-          needle    : $item->id,
+          needle: $item->id,
           column_key: "comment_id"
         );
       }
@@ -120,21 +125,21 @@ class CommentController extends Controller {
   /**
    * Show the form for editing the specified resource.
    */
-  public function edit (string $id): void {
+  public function edit(string $id): void {
     //
   }
 
   /**
    * Update the specified resource in storage.
    */
-  public function update (Request $request, string $id): void {
+  public function update(Request $request, string $id): void {
     //
   }
 
   /**
    * Remove the specified resource from storage.
    */
-  public function destroy (string $id): void {
+  public function destroy(string $id): void {
     //
   }
 }
