8e58e10d7950e3ff94653c91d465ca89c34c124b
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-05 22:54:21 +0300

    refactor: reorder imports and adjust indentation in globalDataStore
    - Moved the `createGlobalState` import above `Ref` and `ref` to follow import grouping conventions.
    - Adjusted the indentation of code blocks to follow consistent spacing.
    - Updated the functions `setActiveProfileOption` and `setActiveNotificationOption` with proper indentation for better readability.
    - Reorganized the return object structure, ensuring uniform indentation for cleaner code presentation.

--- resources/js/stores/globalDataStore.ts
+++ resources/js/stores/globalDataStore.ts
@@ -1,25 +1,25 @@
-import { Ref, ref } from "vue";
-import { createGlobalState } from "@vueuse/core";
 import { NotificationOptions, ProfileOptions } from "@/types/Enums";
+import { createGlobalState } from "@vueuse/core";
+import { Ref, ref } from "vue";
 
 export const useGlobalState = createGlobalState(() => {
-	const activeNotificationOption: Ref<NotificationOptions> = ref(
-		NotificationOptions.All
-	);
-	const activeProfileOption: Ref<ProfileOptions> = ref(ProfileOptions.Posts);
+  const activeNotificationOption: Ref<NotificationOptions> = ref(
+    NotificationOptions.All
+  );
+  const activeProfileOption: Ref<ProfileOptions> = ref(ProfileOptions.Posts);
 
-	function setActiveProfileOption(value: ProfileOptions): void {
-		activeProfileOption.value = value;
-	}
+  function setActiveProfileOption(value: ProfileOptions): void {
+    activeProfileOption.value = value;
+  }
 
-	function setActiveNotificationOption(value: NotificationOptions): void {
-		activeNotificationOption.value = value;
-	}
+  function setActiveNotificationOption(value: NotificationOptions): void {
+    activeNotificationOption.value = value;
+  }
 
-	return {
-		activeNotificationOption,
-		activeProfileOption,
-		setActiveProfileOption,
-		setActiveNotificationOption,
-	};
+  return {
+    activeNotificationOption,
+    activeProfileOption,
+    setActiveProfileOption,
+    setActiveNotificationOption,
+  };
 });
