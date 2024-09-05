be693e215753944f1b0c2dc13f3ad875a688d09b
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-05 23:17:41 +0300

    refactor: enhance type safety in `Feed.vue` with TypeScript
    - Imported `ITweet` and added a `PageProps` interface for page props.
    - Updated `usePage` to use `PageProps` type.
    - Reorganized imports for clarity.
    - Ensured type safety by explicitly defining the `PageProps` interface.

--- resources/js/Components/Feed/Feed.vue
+++ resources/js/Components/Feed/Feed.vue
@@ -1,40 +1,46 @@
 <script lang="ts" setup>
-	import { usePage } from "@inertiajs/vue3";
-	import { useFeedStore } from "@/stores/feedStore.js";
-	import { storeToRefs } from "pinia";
-	import { computed, watch } from "vue";
-	import FeedTweet from "@/Components/Feed/Tweet.vue";
-	import EmptyFeed from "@/Components/Placeholders/EmptyFeed.vue";
+  import FeedTweet from "@/Components/Feed/Tweet.vue";
+  import EmptyFeed from "@/Components/Placeholders/EmptyFeed.vue";
+  import { useFeedStore } from "@/stores/feedStore.js";
+  import { ITweet } from "@/types";
+  import { usePage } from "@inertiajs/vue3";
+  import { storeToRefs } from "pinia";
+  import { computed, watch } from "vue";
 
-	const page = usePage();
+  interface PageProps {
+    feed: ITweet[];
+    [key: string]: any;
+  }
 
-	const feedStore = useFeedStore();
+  const page = usePage<PageProps>();
 
-	// Set the feed data from Inertia props to Pinia store
-	feedStore.setFeed(page.props.feed);
+  const feedStore = useFeedStore();
 
-	const { feed } = storeToRefs(feedStore);
+  // Set the feed data from Inertia props to Pinia store
+  feedStore.setFeed(page.props.feed);
 
-	const feedLength = computed(() => feed.value.length);
+  const { feed } = storeToRefs(feedStore);
 
-	watch(feedLength, () => {
-		feedStore.getFeed();
-	});
+  const feedLength = computed(() => feed.value.length);
+
+  watch(feedLength, () => {
+    feedStore.getFeed();
+  });
 </script>
 
 <template>
-	<section class="grid">
-		<FeedTweet
-			v-for="tweet in feed"
-			v-if="feedLength > 0"
-			:key="tweet.id"
-			:tweet="tweet" />
-		<EmptyFeed v-else />
-	</section>
+  <section class="grid">
+    <FeedTweet
+      v-for="tweet in feed"
+      v-if="feedLength > 0"
+      :key="tweet.id"
+      :tweet="tweet" />
+    <EmptyFeed v-else />
+  </section>
 </template>
 
 <style scoped>
-	section {
-		grid-area: feed;
-	}
+  section {
+    grid-area: feed;
+  }
 </style>
