5c1d19cb0b574c28d5acec4496c360b9d1375318
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-05 23:24:19 +0300

    fix: optimize Profile.vue for better data handling and layout consistency
    - Refactored import statements for improved readability.
    - Changed `page.props` to TypeScript interface for better type checking.
    - Updated `profileStore.setFeed` calls to handle arrays directly.
    - Replaced length checks with `isEmptyArray` utility for consistency.
    - Adjusted template to use `isEmptyArray` for conditional rendering of placeholders.
    - Ensured consistent styling and component usage.

--- resources/js/Pages/Profile/Profile.vue
+++ resources/js/Pages/Profile/Profile.vue
@@ -1,111 +1,108 @@
 <script setup lang="ts">
-	import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-	import ProfileMain from "@/Pages/Profile/Partials/ProfileMain.vue";
-	import Header from "@/Components/Header.vue";
-	import OptionsBar from "@/Components/OptionsBar.vue";
-	import Tweet from "@/Components/Feed/Tweet.vue";
-	import Comment from "@/Components/Comment.vue";
-	import NoTweets from "@/Components/Placeholders/NoTweets.vue";
-	import NoLikes from "@/Components/Placeholders/NoLikes.vue";
-	import NoReplies from "@/Components/Placeholders/NoReplies.vue";
-	import { Head, usePage } from "@inertiajs/vue3";
-	import { useGlobalState } from "@/stores/globalDataStore.js";
-	import { useProfileStore } from "@/stores/profileStore.js";
-	import { computed } from "vue";
-	import { ProfileOptions } from "@/types/Enums";
-	import { ProfileStore, ITweet, User } from "@/types";
+  import Comment from "@/Components/Comment.vue";
+  import Tweet from "@/Components/Feed/Tweet.vue";
+  import Header from "@/Components/Header.vue";
+  import OptionsBar from "@/Components/OptionsBar.vue";
+  import NoLikes from "@/Components/Placeholders/NoLikes.vue";
+  import NoReplies from "@/Components/Placeholders/NoReplies.vue";
+  import NoTweets from "@/Components/Placeholders/NoTweets.vue";
+  import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+  import { isEmptyArray } from "@/lib/Helpers";
+  import ProfileMain from "@/Pages/Profile/Partials/ProfileMain.vue";
+  import { useGlobalState } from "@/stores/globalDataStore.js";
+  import { useProfileStore } from "@/stores/profileStore.js";
+  import { IComment, ITweet, ProfileStore, User } from "@/types";
+  import { ProfileOptions } from "@/types/Enums";
+  import { Head, usePage } from "@inertiajs/vue3";
+  import { computed } from "vue";
 
-	const page = usePage();
+  interface PageProps {
+    allPosts: ITweet[];
+    likedPosts: ITweet[];
+    replies: IComment[];
+    user: User;
+    [key: string]: any;
+  }
 
-	const profileStore: ProfileStore = useProfileStore();
-	const globalState = useGlobalState();
+  const page = usePage<PageProps>();
 
-	profileStore.setFeed(ProfileOptions.Posts, page.props.allPosts as ITweet[]);
-	profileStore.setFeed(
-		ProfileOptions.Likes,
-		page.props.likedPosts as ITweet[]
-	);
-	profileStore.setFeed(
-		ProfileOptions.Replies,
-		page.props.replies as ITweet[]
-	);
+  const profileStore: ProfileStore = useProfileStore();
+  const globalState = useGlobalState();
 
-	const user = computed(() => page.props.user as User);
+  profileStore.setFeed(ProfileOptions.Posts, page.props.allPosts);
+  profileStore.setFeed(ProfileOptions.Likes, page.props.likedPosts);
+  profileStore.setFeed(ProfileOptions.Replies, page.props.replies);
 
-	console.log(user.value, "user");
+  const user = computed(() => page.props.user);
 
-	// console.log(toRaw(profileStore.posts));
-	// console.log(toRaw(profileStore.liked));
-	// console.log(toRaw(profileStore.replies));
+  console.log(user.value, "user");
+
+  // console.log(toRaw(profileStore.posts));
+  // console.log(toRaw(profileStore.liked));
+  // console.log(toRaw(profileStore.replies));
 </script>
 
 <template>
-	<AuthenticatedLayout>
-		<Header
-			:backable="true"
-			:title="user.full_name" />
-		<Head :title="`${user.full_name} | (@${user.username})`" />
-		<ProfileMain />
-		<section class="grid">
-			<OptionsBar
-				:options="['posts', 'replies', 'likes']"
-				type="profile" />
-			<Tweet
-				v-for="tweet in profileStore.posts"
-				v-if="
-					profileStore.posts.length !== 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Posts
-				"
-				:key="tweet.id"
-				:tweet="tweet" />
+  <AuthenticatedLayout>
+    <Header
+      :backable="true"
+      :title="user.full_name" />
+    <Head :title="`${user.full_name} | (@${user.username})`" />
+    <ProfileMain />
+    <section class="grid">
+      <OptionsBar
+        :options="['posts', 'replies', 'likes']"
+        type="profile" />
+      <Tweet
+        v-for="tweet in profileStore.posts"
+        v-if="
+          !isEmptyArray(profileStore.posts) &&
+          globalState.activeProfileOption.value === ProfileOptions.Posts
+        "
+        :key="tweet.id"
+        :tweet="tweet" />
 
-			<Comment
-				v-for="comment in profileStore.replies"
-				v-if="
-					profileStore.replies.length !== 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Replies
-				"
-				:key="comment.id"
-				:comment="comment" />
+      <Comment
+        v-for="comment in profileStore.replies"
+        v-if="
+          !isEmptyArray(profileStore.replies) &&
+          globalState.activeProfileOption.value === ProfileOptions.Replies
+        "
+        :key="comment.id"
+        :comment="comment" />
 
-			<Tweet
-				v-for="tweet in profileStore.liked"
-				v-if="
-					profileStore.liked.length !== 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Likes
-				"
-				:key="tweet.id"
-				:tweet="tweet" />
+      <Tweet
+        v-for="tweet in profileStore.liked"
+        v-if="
+          !isEmptyArray(profileStore.liked) &&
+          globalState.activeProfileOption.value === ProfileOptions.Likes
+        "
+        :key="tweet.id"
+        :tweet="tweet" />
 
-			<NoTweets
-				v-if="
-					profileStore.posts.length === 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Posts
-				"
-				:full_name="user.full_name"
-				class="my-5" />
+      <NoTweets
+        v-if="
+          isEmptyArray(profileStore.posts) &&
+          globalState.activeProfileOption.value === ProfileOptions.Posts
+        "
+        :full_name="user.full_name"
+        class="my-5" />
 
-			<NoReplies
-				v-if="
-					profileStore.replies.length === 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Replies
-				"
-				:full_name="user.full_name"
-				class="my-5" />
+      <NoReplies
+        v-if="
+          isEmptyArray(profileStore.replies) &&
+          globalState.activeProfileOption.value === ProfileOptions.Replies
+        "
+        :full_name="user.full_name"
+        class="my-5" />
 
-			<NoLikes
-				v-if="
-					profileStore.liked.length === 0 &&
-					globalState.activeProfileOption.value ===
-						ProfileOptions.Likes
-				"
-				:full_name="user.full_name"
-				class="my-5" />
-		</section>
-	</AuthenticatedLayout>
+      <NoLikes
+        v-if="
+          isEmptyArray(profileStore.liked) &&
+          globalState.activeProfileOption.value === ProfileOptions.Likes
+        "
+        :full_name="user.full_name"
+        class="my-5" />
+    </section>
+  </AuthenticatedLayout>
 </template>
