d7546500471073dd5c401fd56b5295488480593b
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-05 23:20:34 +0300

    refactor: clean up and enhance AuthenticatedLayout.vue
    - Reordered import statements for better readability.
    - Updated `PageProps` interface to include `User` type and any additional properties.
    - Removed redundant type casting in `authStore.setAuthenticatedUser`.
    - Added `authStore.fetchUser()` call for fetching user data.
    - Reformatted `section` style block for consistency.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,37 +1,44 @@
 <script lang="ts" setup>
-	import { useAuthStore } from "@/stores/authStore";
-	import { usePage } from "@inertiajs/vue3";
-	import SideBar from "../Components/SideBar.vue";
-	import FollowBar from "@/Components/FollowBar.vue";
-	import { User } from "@/types";
+  import FollowBar from "@/Components/FollowBar.vue";
+  import { useAuthStore } from "@/stores/authStore";
+  import { User } from "@/types";
+  import { usePage } from "@inertiajs/vue3";
+  import SideBar from "../Components/SideBar.vue";
 
-	const page = usePage();
+  interface PageProps {
+    user: User;
+    [key: string]: any;
+  }
 
-	const authStore = useAuthStore();
+  const page = usePage<PageProps>();
 
-	authStore.setAuthenticatedUser(page.props.auth.user as unknown as User);
+  const authStore = useAuthStore();
+
+  authStore.setAuthenticatedUser(page.props.auth.user);
+
+  authStore.fetchUser();
 </script>
 
 <template>
-	<section id="Home">
-		<SideBar />
-		<slot />
-		<FollowBar />
-	</section>
+  <section id="Home">
+    <SideBar />
+    <slot />
+    <FollowBar />
+  </section>
 </template>
 
 <style scoped>
-	section {
-		grid-template-areas:
-			"nav header followBar"
-			"nav tweet followBar"
-			"nav feed followBar";
+  section {
+    grid-template-areas:
+      "nav header followBar"
+      "nav tweet followBar"
+      "nav feed followBar";
 
-		display: grid;
-		/* grid-template-columns: auto 1fr 24rem; */
-		grid-template-rows: auto auto 1fr;
+    display: grid;
+    /* grid-template-columns: auto 1fr 24rem; */
+    grid-template-rows: auto auto 1fr;
 
-		@apply lg:grid-cols-[auto_1fr_24rem]
+    @apply lg:grid-cols-[auto_1fr_24rem]
     grid-cols-[auto_1fr_20rem];
-	}
+  }
 </style>
