376c1fae12ed1efa28a02fe03932b06ec792dc2f
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-05 23:16:32 +0300

    style: adjust formatting in `Tooltip.vue`
    - Removed unnecessary space before the closing `>` in the `class` attribute of `Tooltip.vue`.

--- resources/js/Components/Tooltip.vue
+++ resources/js/Components/Tooltip.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-	const props = defineProps<{
+	defineProps<{
 		message: string;
 		visible: boolean;
 	}>();
@@ -11,7 +11,7 @@
 			'opacity-100': $props.visible,
 			'opacity-0': !$props.visible,
 		}"
-		class="transition duration-500 ease-in-out bg-white text-black absolute top-full left-1/2 z-20 mt-1.5 -translate-x-1/2 whitespace-nowrap rounded-md py-1.5 px-2 text-sm ">
+		class="transition duration-500 ease-in-out bg-white text-black absolute top-full left-1/2 z-20 mt-1.5 -translate-x-1/2 whitespace-nowrap rounded-md py-1.5 px-2 text-sm">
 		<span
 			class="absolute top-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45" />
 		{{ $props.message }}
