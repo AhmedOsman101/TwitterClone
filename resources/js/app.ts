c53c216b968a7517a149e57636a63bdb6100a903
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-05-30 00:54:49 +0300

    refactor: move axios and add utils
    - Moved axios instance
    - Added utils functions
    - Added SSR setup

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,34 +1,53 @@
-import "./bootstrap";
 import "../css/app.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "vue3-emoji-picker/css";
 
-import { createApp, h, DefineComponent } from "vue";
 import { createInertiaApp, Link } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 import { createPinia } from "pinia";
+import type { DefineComponent } from "vue";
+import { createApp, h } from "vue";
+import { ZiggyVue } from "ziggy-js";
+import { initializeTheme } from "./composables/useAppearance";
+import AxiosInstance from "./lib/axios";
+
+// @ts-expect-error Extend ImportMeta interface for Vite...
+declare module "vite/client" {
+  interface ImportMetaEnv {
+    readonly VITE_APP_NAME: string;
+    [key: string]: string | boolean | undefined;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+    readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
+  }
+}
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 const pinia = createPinia();
+window.axios = AxiosInstance;
 
 createInertiaApp({
-	title: (title) => `${title ?? appName}`,
-	resolve: (name) =>
-		resolvePageComponent(
-			`./Pages/${name}.vue`,
-			import.meta.glob<DefineComponent>("./Pages/**/*.vue")
-		),
-	setup({ el, App, props, plugin }) {
-		createApp({ render: () => h(App, props) })
-			.use(plugin)
-			.use(ZiggyVue)
-			.use(pinia)
-			.component("Link", Link)
-			.mount(el);
-	},
-	progress: {
-		color: "#38bdf8",
-		showSpinner: true,
-	},
+  title: title => `${title ?? appName}`,
+  resolve: name =>
+    resolvePageComponent(
+      `./pages/${name}.vue`,
+      import.meta.glob<DefineComponent>("./pages/**/*.vue")
+    ),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue)
+      .use(pinia)
+      .component("Link", Link)
+      .mount(el);
+  },
+  progress: {
+    color: "#38bdf8",
+    showSpinner: true,
+  },
 });
+
+// This will set light / dark mode on page load...
+initializeTheme();
--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 /**
  * The function `isEqualObjects` compares two objects recursively to determine if they are equal in
  * terms of keys and values.
@@ -6,10 +13,10 @@
  * @returns Returns `true` if the two input objects are equal in terms
  * of their keys and values, and `false` otherwise.
  */
-export const isEqualObjects = (
-  firstObject: Record<string, any>,
-  secondObject: Record<string, any>
-): boolean => {
+export function isEqualObjects(
+  firstObject: unknown,
+  secondObject: unknown
+): boolean {
   // If both are strictly equal, return true
   if (firstObject === secondObject) return true;
 
@@ -27,12 +34,12 @@ export const isEqualObjects = (
   const keys1 = Object.keys(firstObject);
 
   // Check if all keys and their values are equal
-  return keys1.every((key) => {
+  return keys1.every(key => {
     // If the key does not exist in secondObject, return false
-    if (!secondObject.hasOwnProperty(key)) return false;
+    if (!Object.hasOwn(secondObject, key)) return false;
 
-    const val1 = firstObject[key];
-    const val2 = secondObject[key];
+    const val1 = (firstObject as Record<PropertyKey, unknown>)[key];
+    const val2 = (secondObject as Record<PropertyKey, unknown>)[key];
 
     // Check if both values are objects and recursively compare
     const areBothObjects =
@@ -43,7 +50,7 @@ export const isEqualObjects = (
 
     return areBothObjects ? isEqualObjects(val1, val2) : val1 === val2;
   });
-};
+}
 
 /**
  * The `formatNumber` function converts a large number into a more readable
@@ -53,17 +60,19 @@ export const isEqualObjects = (
  * @returns Returns a formatted string representing the number in a
  * shortened format with a suffix indicating the magnitude.
  */
-export const formatNumber = (num: number): string => {
-  if (num >= 1000000000) {
-    return (num / 1000000000).toFixed(num % 1000000000 === 0 ? 0 : 1) + "B";
-  } else if (num >= 1000000) {
-    return (num / 1000000).toFixed(num % 1000000 === 0 ? 0 : 1) + "M";
-  } else if (num >= 1000) {
-    return (num / 1000).toFixed(num % 1000 === 0 ? 0 : 1) + "K";
-  } else {
-    return num.toString();
+export function formatNumber(num: number): string {
+  if (num >= 1_000_000_000) {
+    return `${(num / 1_000_000_000).toFixed(num % 1_000_000_000 === 0 ? 0 : 1)}B`;
   }
-};
+  if (num >= 1000000) {
+    return `${(num / 1_000_000).toFixed(num % 1_000_000 === 0 ? 0 : 1)}M`;
+  }
+  if (num >= 1000) {
+    return `${(num / 1000).toFixed(num % 1000 === 0 ? 0 : 1)}K`;
+  }
+
+  return num.toString();
+}
 
 /**
  * The function `getComponent` extracts the last part of a component path from a
@@ -74,31 +83,27 @@ export const formatNumber = (num: number): string => {
  * @returns The last part of the `component` string after splitting it
  * by "/".
  */
-export const getComponent = (page: { component: string }): string => {
+export function getComponent(page: { component: string }): string {
   const component = page.component;
 
-  const splittedComponent = component.split("/");
+  const splitted = component.split("/");
 
-  const index = splittedComponent.length - 1;
-
-  const result = splittedComponent[index];
-
-  return result;
-};
+  return splitted[splitted.length - 1];
+}
 
 /**
  * Truncates a string to a specified limit and appends "..." if the string exceeds the limit.
- * @param string - The input string that you want to limit in terms of length.
- * @param  limit - The maximum length that the input `string` should be truncated to. If the length of the input `string` exceeds this `limit`, it will be truncated and "..." will be appended to indicate that it has been shortened.
+ * @param str - The input string that you want to limit in terms of length.
+ * @param limit - The maximum length that the input `string` should be truncated to. If the length of the input `string` exceeds this `limit`, it will be truncated and "..." will be appended to indicate that it has been shortened.
  * @returns The truncated string or the original string if length is below limit.
  */
-export const StringLimit = (string: string, limit: number): string => {
-  if (string.length > limit) {
-    return `${string.substring(0, limit - 3)}...`;
+export function StringLimit(str: string, limit: number): string {
+  if (str.length > limit) {
+    return `${str.substring(0, limit - 3)}...`;
   }
 
-  return string;
-};
+  return str;
+}
 
 /**
  * Checks if an array is empty without using the length property.
@@ -106,12 +111,9 @@ export const StringLimit = (string: string, limit: number): string => {
  * @param array - The array to check for emptiness.
  * @returns Returns true if the array is empty, false otherwise.
  */
-export const isEmptyArray = <T>(array: T[]): boolean => {
-  // Use a for...in loop to iterate over array indices
-  for (let i in array) {
-    // If we enter this loop, the array has at least one element
-    return false;
-  }
+export function isEmptyArray(array: unknown[]): boolean {
+  // If we enter this loop, the array has at least one element
+  for (const _ in array) return false;
   // If the loop never executed, the array is empty
   return true;
-};
+}
