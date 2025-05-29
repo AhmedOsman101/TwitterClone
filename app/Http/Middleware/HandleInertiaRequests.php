0e59860cd9be5f1ddff12b16077cd90da7b02cfb
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-05-30 00:39:33 +0300

    feat: add appearance middleware
    - Shares appearance with all views
    - Sets default to 'system'

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,37 +3,53 @@
 namespace App\Http\Middleware;
 
 use App\Http\Resources\AuthUserResource;
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
+use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware {
   /**
-   * The root template that is loaded on the first page visit.
+   * The root template that's loaded on the first page visit.
+   *
+   * @see https://inertiajs.com/server-side-setup#root-template
    *
    * @var string
    */
   protected $rootView = 'app';
 
   /**
-   * Determine the current asset version.
+   * Determines the current asset version.
+   *
+   * @see https://inertiajs.com/asset-versioning
    */
-  public function version(Request $request): string|null {
+  public function version(Request $request): ?string {
     return parent::version($request);
   }
 
   /**
    * Define the props that are shared by default.
    *
+   * @see https://inertiajs.com/shared-data
+   *
    * @return array<string, mixed>
    */
   public function share(Request $request): array {
+    [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
 
     return [
       ...parent::share($request),
-      'auth' => [
-        'user' => Auth::check() ? AuthUserResource::make(Auth::user())->resolve() : null,
+      'name'        => config('app.name'),
+      'quote'       => ['message' => trim($message), 'author' => trim($author)],
+      'auth'        => [
+        'user' => Auth::check() ? AuthUserResource::make($request->user())->resolve() : null,
       ],
+      'ziggy'       => [
+        ...(new Ziggy)->toArray(),
+        'location' => $request->url(),
+      ],
+      'sidebarOpen' => !$request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
     ];
   }
 }
