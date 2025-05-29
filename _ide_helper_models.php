c92affdd69b7311d5d66cec3bb349aa4e5d43315
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-05-29 23:23:06 +0300

    build: add IDE helper file
    - Added _ide_helper.php for autocompletion

---
+++ _ide_helper_models.php
@@ -0,0 +1,163 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $userId
+ * @property int $tweetId
+ * @property string $body
+ * @property \Illuminate\Support\Carbon $createdAt
+ * @property string|null $notificationId
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Like> $likes
+ * @property-read int|null $likes_count
+ * @property-read \App\Models\Tweet|null $tweet
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\CommentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereNotificationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereTweetId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereUserId($value)
+ */
+	class Comment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $followerId
+ * @property int $followedUserId
+ * @property string|null $notificationId
+ * @property-read \App\Models\User|null $followed
+ * @property-read \App\Models\User|null $follower
+ * @method static \Database\Factories\FollowerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower whereFollowedUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower whereFollowerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Follower whereNotificationId($value)
+ */
+	class Follower extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $userId
+ * @property int|null $tweetId
+ * @property int|null $commentId
+ * @property string|null $notificationId
+ * @property \Illuminate\Support\Carbon $createdAt
+ * @property-read \App\Models\Comment|null $comment
+ * @property-read \App\Models\Tweet|null $tweet
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\LikeFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereCommentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereNotificationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereTweetId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Like whereUserId($value)
+ */
+	class Like extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $userId
+ * @property string $body
+ * @property \Illuminate\Support\Carbon $createdAt
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Like> $likes
+ * @property-read int|null $likes_count
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\TweetFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tweet whereUserId($value)
+ */
+	class Tweet extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $fullName
+ * @property string $username
+ * @property string $email
+ * @property string|null $bio
+ * @property string $profilePicture
+ * @property string $coverPhoto
+ * @property string $password
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon $createdAt
+ * @property \Illuminate\Support\Carbon $updatedAt
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Follower> $followers
+ * @property-read int|null $followers_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Follower> $following
+ * @property-read int|null $following_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Like> $likes
+ * @property-read int|null $likes_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tweet> $tweets
+ * @property-read int|null $tweets_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereBio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCoverPhoto($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereFullName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereProfilePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUsername($value)
+ */
+	class User extends \Eloquent {}
+}
+
